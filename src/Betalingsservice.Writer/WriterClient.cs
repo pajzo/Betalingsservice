6d0c0936954964938100b140b1bb38caeeb78439
Peter Winkler <pwr@pi-applications.dk>
2020-06-20 15:21:58 +0200

    Prepared writer

---
+++ src/Betalingsservice.Writer/WriterClient.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Betalingsservice.Writer
+{
+    public static class WriterClient
+    {
+        public static string RegistrationOfMandates()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string CancellationOfMandates()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

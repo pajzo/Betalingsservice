617fecfab5c9b490986f9a8bd55a66ec38fb8859
Peter Winkler <pwr@pi-applications.dk>
2020-06-20 13:50:41 +0200

    Splitted parser and test console

---
+++ src/Betalingsservice.Parser/ParserClient.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Betalingsservice.Parser
+{
+    public static class ParserClient
+    {
+        public static IEnumerable<MandateInformation> GetMandateInformation(string[] fileLines)
+        {
+            var mandateInfoList = fileLines
+                .Where(x => !string.IsNullOrEmpty(x))
+                .Where(x => x.Substring(2, 3) == "042") // Data record type
+                .Select(line => new MandateInformation
+                {
+                    PbsCreditorNo = line.Substring(5, 8),
+                    TransactionCode = line.Substring(13, 4),
+                    DebtorGroupNo = line.Substring(20, 5),
+                    DebtorCustomerNo = line.Substring(25, 15),
+                    MandateNo = line.Substring(40, 9),
+                    Date = ParseDate(line.Substring(49, 6)),
+                    EndDate = ParseNullableDate(line.Substring(55, 6))
+                });
+
+            return mandateInfoList;
+        }
+
+        public static IEnumerable<PaymentInformation> GetPaymentInformation(string[] fileLines)
+        {
+            var paymentInfoList = fileLines
+                .Where(x => !string.IsNullOrEmpty(x))
+                .Where(x => x.Substring(2, 3) == "042") // Data record type
+                .Select(line => new PaymentInformation
+                {
+                    PbsCreditorNo = line.Substring(5, 8),
+                    TransactionCode = line.Substring(13, 4),
+                    DebtorGroupNo = line.Substring(20, 5),
+                    DebtorCustomerNo = line.Substring(25, 15),
+                    MandateNo = line.Substring(40, 9),
+                    Date = ParseDate(line.Substring(49, 6)),
+                    PaymentDate = ParseNullableDate(line.Substring(103, 6)),
+                    BookkeepingEntryDate = ParseNullableDate(line.Substring(109, 6)),
+                    CreditorReference = line.Substring(69, 30),
+                    Amount = decimal.Parse(line.Substring(56, 13)) / 100,
+                    AmountPayment = decimal.Parse(line.Substring(115, 13)) / 100
+                });
+
+            return paymentInfoList;
+        }
+
+        internal static DateTime? ParseNullableDate(string text)
+        {
+            if (text == "000000")
+                return null;
+
+            return ParseDate(text);
+        }
+
+        internal static DateTime ParseDate(string text)
+        {
+            return DateTime.ParseExact(text, "ddMMyy", null);
+        }
+    }
+}
--- src/Betalingsservice.TestConsole/Program.cs
+++ src/Betalingsservice.TestConsole/Program.cs
@@ -1,16 +1,18 @@
 ﻿using Azure.Storage.Blobs;
-using Azure.Storage.Blobs.Models;
+using Betalingsservice.Parser;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 
-namespace Betalingsservice.Parser
+namespace Betalingsservice.TestConsole
 {
     class Program
     {
+        /// <summary>
+        /// This is only demo program
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             var blobServiceClient = new BlobServiceClient("UseDevelopmentStorage=true");
@@ -47,10 +49,10 @@ namespace Betalingsservice.Parser
                             Parse621(lines);
                             break;
                         case "602": // Payment info
-                            var paymentInfo = GetPaymentInformation(lines);
+                            var paymentInfo = ParserClient.GetPaymentInformation(lines);
                             break;
                         case "603": // Mandate info
-                            var mandateInfo = GetMandateInformation(lines);
+                            var mandateInfo = ParserClient.GetMandateInformation(lines);
                             break;
                         default:
                             Console.WriteLine("> Unsupported file type");
@@ -70,61 +72,6 @@ namespace Betalingsservice.Parser
 
             Console.WriteLine("- The end -");
             Console.ReadKey();
-        }
-
-        static IEnumerable<MandateInformation> GetMandateInformation(string[] fileLines)
-        {
-            var mandateInfoList = fileLines
-                .Where(x => !string.IsNullOrEmpty(x))
-                .Where(x => x.Substring(2, 3) == "042") // Data record type
-                .Select(line => new MandateInformation
-                {
-                    PbsCreditorNo = line.Substring(5, 8),
-                    TransactionCode = line.Substring(13, 4),
-                    DebtorGroupNo = line.Substring(20, 5),
-                    DebtorCustomerNo = line.Substring(25, 15),
-                    MandateNo = line.Substring(40, 9),
-                    Date = ParseDate(line.Substring(49, 6)),
-                    EndDate = ParseNullableDate(line.Substring(55, 6))
-                });
-
-            return mandateInfoList;
-        }
-
-        static IEnumerable<PaymentInformation> GetPaymentInformation(string[] fileLines)
-        {
-            var paymentInfoList = fileLines
-                .Where(x => !string.IsNullOrEmpty(x))
-                .Where(x => x.Substring(2, 3) == "042") // Data record type
-                .Select(line => new PaymentInformation
-                {
-                    PbsCreditorNo = line.Substring(5, 8),
-                    TransactionCode = line.Substring(13, 4),
-                    DebtorGroupNo = line.Substring(20, 5),
-                    DebtorCustomerNo = line.Substring(25, 15),
-                    MandateNo = line.Substring(40, 9),
-                    Date = ParseDate(line.Substring(49, 6)),
-                    PaymentDate = ParseNullableDate(line.Substring(103, 6)),
-                    BookkeepingEntryDate = ParseNullableDate(line.Substring(109, 6)),
-                    CreditorReference = line.Substring(69, 30),
-                    Amount = decimal.Parse(line.Substring(56, 13)) / 100,
-                    AmountPayment = decimal.Parse(line.Substring(115, 13)) / 100
-                });
-
-            return paymentInfoList;
-        }
-
-        static DateTime? ParseNullableDate(string text)
-        {
-            if (text == "000000")
-                return null;
-
-            return ParseDate(text);
-        }
-
-        static DateTime ParseDate(string text)
-        {
-            return DateTime.ParseExact(text, "ddMMyy", null);
         }
 
         /// <summary>
